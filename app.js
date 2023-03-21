564cfea861ed409566874a909e90ea1063bbdd7b
pkmin11 <126228012+pkmin11@users.noreply.github.com>
2023-03-21 22:17:12 +0900

    day2

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-alert("hi");
+/*const a =10;
+const b= 2;
+const myName = "nico";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello "+ myName);3
+
+const amIFat = null;
+let something;
+console.log(something, amIFat);
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = ["mon" , "thu" , "wed" , "thu" , "fri" , "sat"];
+console.log(daysOfWeek);
+// Get Item from Array
+console.log(daysOfWeek[5]);
+console.log(daysOfWeek[4]); 
+ 
+
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "nico"];
+console.log(nonsense);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek)
+
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[2]);
+toBuy.push("kimbab");
+console.log(toBuy[3]);;*/
+
+/*
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "Little bit"
+
+player.name
+player.points
+player.handsome 
+
+// player[0] == name ...
+const player = ["nico", 121212, false, "Little bit"]*/
+
+const player = {
+    name : "nico", 
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player);
