933e59d291e422d1b255041d4a9d8180cd34941c
pkmin11 <126228012+pkmin11@users.noreply.github.com>
2023-04-01 23:15:44 +0900

    day 11

--- app4.js
+++ app4.js
@@ -2,14 +2,26 @@
 const h1 =document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
+   /* const currentColor = h1.style.color;
     let newColor;
     if(currentColor === "blue"){
         newColor = "tomato";
     } else {
         newColor = "blue";
     }
-    h1.style.color = newColor;
+    h1.style.color = newColor;*/
+    
+    /*const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)){
+    h1.classList.remove(clickedClass);
+    } else {
+        h1.classList.add(clickedClass);
+    } = */
+
+h1.classList.toggle("clicked");
+// classList에 clicked가 존재하면 remove
+// 존재하지 않으면 clicked를 add 해주는 역할을 한다
+    
 }
 
 h1.addEventListener("click", handleTitleClick);
--- assignment.js
+++ assignment.js
@@ -8,7 +8,7 @@ title의 색상은 colors 배열에 있는 색을 사용해야 합니다.
 모든 함수 핸들러는 superEventHandler내부에 작성해야 합니다.
 모든 조건이 충족되지 못하면 ❌를 받습니다.*/
 
-
+/*
 const h1 =document.querySelector("div.hello:first-child h1");
 
 
@@ -38,3 +38,50 @@ h1.addEventListener("mouseleave", handleMouseLeave);
 
 window.addEventListener("resize", handleWindowResize);
 window.addEventListener("contextmenu", handleWindowcontextmenu);
+*/
+
+// 정답
+
+import "./styles.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+/*
+✅ The text of the title should change when the mouse is on top of it.
+✅ The text of the title should change when the mouse is leaves it.
+✅ When the window is resized the title should change.
+✅ On right click the title should also change.
+✅ The colors of the title should come from a color from the colors array.
+✅ DO NOT CHANGE .css, or .html files.
+✅ ALL function handlers should be INSIDE of "superEventHandler"
+*/
+
+const h2 = document.querySelector("h2"); // h2선언
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    h2.style.color = colors[0];
+    h2.innerText = "The Mouse is here!";
+  },
+
+  handleMouseLeave: function () {
+    h2.style.color = colors[1];
+    h2.innerText = "The Mouse is gone!";
+  },
+
+  handleWindowResize: function () {
+    h2.style.color = colors[2];
+    h2.innerText = "You just Resized!";
+  },
+
+  handleWindowcontextmenu: function () {
+    h2.style.color = colors[4];
+    h2.innerText = "That was a right click!";
+  }
+};
+
+h2.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+h2.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener("contextmenu", superEventHandler.handleWindowcontextmenu);
